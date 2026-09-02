6a71293216f25830e03f321ab11aed9da27b2c63
Ankit Patidar <ankit.iesipstechclub@gmail.com>
2026-09-02 23:32:40 +0530

    fix cors for production

--- server/index.js
+++ server/index.js
@@ -17,10 +17,23 @@ import addressRouter from './route/address.route.js'
 import orderRouter from './route/order.route.js'
 
 const app = express()
+// app.use(cors({
+//     credentials : true,
+//     origin : process.env.FRONTEND_URL
+// }))
+
+const allowedOrigins = process.env.FRONTEND_URL.split(",");
+
 app.use(cors({
-    credentials : true,
-    origin : process.env.FRONTEND_URL
-}))
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true
+}));
 app.use(express.json())
 app.use(cookieParser())
 app.use(morgan("dev"));
